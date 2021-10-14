1e633bab5f3f9e89f84fbe375e9ac6968b3e0f9e
lindaerin <lindaerin00@gmail.com>
2021-10-13 23:43:35 -0400

    add route to home page

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,19 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomePage from "./pages/HomePage";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <main className="py-3"> 
+      <main className="py-3">
         <Container>
-          <HomePage></HomePage>
+          <Route path="/" component={HomePage} exact />
         </Container>
       </main>
-
       <Footer />
-    </div>
+    </Router>
   );
 }
 
