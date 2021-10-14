fc10c533909d3dbecad65463dc5682428eaaf428
lindaerin <lli007@citymail.cuny.edu>
2021-10-14 17:39:08 -0400

    update to es modules in node.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-// Intialize express
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import dotenv from "dotenv";
+import products from "./data/products.js";
 
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
@@ -17,4 +18,9 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server runnning on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server runnning in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
