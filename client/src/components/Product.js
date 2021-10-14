8713fe3ca810bc7bf7fbc2e7a1a26f03334d4d33
lindaerin <lindaerin00@gmail.com>
2021-10-13 23:13:57 -0400

    add product ratings section

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,29 +1,34 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import Rating from "./Rating";
+import "../index.css";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-4 p-3 rounded">
       <a href={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant='top' />
+        <Card.Img src={product.image} variant="top" />
       </a>
 
       <Card.Body as="h6">
-        <a href={`/product/${product._id}`}></a>
+        <a href={`/product/${product._id}`} className="cardlink">
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
+        </a>
 
         <Card.Text as="div">
           <div className="my-3">
-            {product.rating} from {product.numReviews} reviews
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
           </div>
         </Card.Text>
 
         <Card.Text as="h2">
           <div className="my-3">${product.price}</div>
         </Card.Text>
-
       </Card.Body>
     </Card>
   );
